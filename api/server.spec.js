78e289cdd3b0d28225cdaf9a7450b86d2db901fc
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-19 20:19:01 -0800

    completed most of the project. still running into testing bug. will solve later

---
+++ api/server.spec.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+
+const server = require("./server.js");
+
+describe("server.js", () => {
+  describe("environment", () => {
+    it("should set env to testing", () => {
+      expect(process.env.DB_ENV).toBe("testing");
+    });
+  });
+  describe("GET /", () => {
+    it("should return a 200 OK", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+    it("should return a JSON object", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+  });
+});
